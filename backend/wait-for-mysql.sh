cf7c17ae7e3cb58d8b09449ff97516d219eec651
noa eisnkut <noaeisnkut@gmail.com>
2025-09-10 15:48:02 +0300

    fixed wait-for... file

--- backend/wait-for-mysql.sh
+++ backend/wait-for-mysql.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-if [ -z "$DB_HOST" ]; then
-  echo "DB_HOST is not set"
-  exit 1
-fi
-
-until mysqladmin ping -h"$DB_HOST" --silent; do
+until mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
     echo "Waiting for MySQL..."
     sleep 2
 done
-
 exec "$@"
