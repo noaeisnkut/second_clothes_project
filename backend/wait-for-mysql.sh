2ed998dca3f1805bb01fcef975d22cac8e579eb0
noa eisnkut <noaeisnkut@gmail.com>
2025-09-10 16:43:30 +0300

    changed docker-compose

--- backend/wait-for-mysql.sh
+++ backend/wait-for-mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 until mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
-    echo "Waiting for MySQL..."
-    sleep 2
+   echo "Waiting for MySQL..."
+   sleep 2
 done
 exec "$@"
