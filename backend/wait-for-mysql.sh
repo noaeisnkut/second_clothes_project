20c5fd84dd7a99cb110aaf7e75eaa9454f96e888
noa eisnkut <noaeisnkut@gmail.com>
2025-09-10 16:55:59 +0300

    changed docker-compose, wait file and ci.yml

--- backend/wait-for-mysql.sh
+++ backend/wait-for-mysql.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+# Debug info
+echo "Waiting for MySQL at host: $DB_HOST, user: $DB_USER"
+
+# מחכה עד שה-MySQL מוכן
 until mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
    echo "Waiting for MySQL..."
    sleep 2
 done
+
+echo "MySQL is up!"
 exec "$@"
